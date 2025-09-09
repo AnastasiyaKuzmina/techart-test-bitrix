3ba42bea9936b37f512e8345939fd70bad0cd163
Anastasiya Kuzmina <anastasiya.kuzmina.0202@mail.ru>
2025-09-09 16:49:37 +0300

    Add blocks

---
+++ project/src/block/common/header/header.blade.php
@@ -0,0 +1,19 @@
+<div class="{{ $block }}">
+    <div class="{{ $block->elem('top-container') }}">
+        <div class="{{ $block->elem('left-content') }}">
+            <img class="{{ $block->elem('logo') }}" src="{{ \TAO::frontendUrl('img/icons/logo.svg') }}" alt="">
+            <p>Галактический вестник</p>
+        </div>
+        <div class="{{ $block->elem('right-content') }}">
+            {!! $topMenu !!}
+            @if ($user->IsAuthorized())
+                <div class="{{ $block->elem('auth-menu') }}">
+                    <p class="{{ $block->elem('auth-item')->mod('user-name') }}">{{ $user->GetFullName() }}</p>
+                    <a class="{{ $block->elem('auth-item') }}" href="/?logout=yes&{{ $sessionId }}">Выйти</a>
+                </div>
+            @endif
+        </div>
+    </div>
+    {!! $leftMenu !!}
+    <hr>
+</div>
